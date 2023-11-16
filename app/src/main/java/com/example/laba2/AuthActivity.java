8026f80058e880f34391ce70dc7244dabc7e6222
mlgzilla <rmlgzilla@gmail.com>
2023-11-16 12:03:54 +0300

    kjsdjsdkj

--- app/src/main/java/com/example/laba2/AuthActivity.java
+++ app/src/main/java/com/example/laba2/AuthActivity.java
@@ -1,78 +1,103 @@
 package com.example.laba2;
 
-import android.content.Intent;
+import android.content.ContentValues;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.content.ContextCompat;
+
+import com.example.laba2.databinding.AuthActivityBinding;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class AuthActivity extends AppCompatActivity {
-    DatabaseHandler db = new DatabaseHandler(this);
+    private AuthActivityBinding activityBinding;
+
+    private ProcessCameraProvider cameraProvider;
+    private ImageCapture imageCapture;
 
     @Override
     protected void onStart() {
         super.onStart();
-        setContentView(R.layout.auth_activity);
+        activityBinding = AuthActivityBinding.inflate(getLayoutInflater());
+        setContentView(activityBinding.getRoot());
 
-        Button submitButton = findViewById(R.id.submitButton);
-        Button regButton = findViewById(R.id.regButton);
-        EditText loginText = findViewById(R.id.editTextLogin);
-        EditText passwdText = findViewById(R.id.editTextPassword);
-        Toast myToast = new Toast(this);
-        Intent openIntent = new Intent(this, ListActivity.class);
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        activityBinding.shootButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        loginText.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Integer userId = db.loginUser(loginText.getText().toString(), passwdText.getText().toString());
-                                if (userId != null) {
-                                    openIntent.putExtra("userLogin", loginText.getText().toString());
-                                    startActivity(openIntent);
-                                } else
-                                    Toast.makeText(AuthActivity.this, "Wrong login or password, try again.", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-                }).start();
+                capturePhoto();
             }
         });
-        regButton.setOnClickListener(new View.OnClickListener() {
+
+        ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderListenableFuture.addListener(new Runnable() {
             @Override
-            public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        loginText.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Integer userId = db.authUser(loginText.getText().toString());
-                                if (userId == null) {
-                                    User user = new User();
-                                    user.setLogin(loginText.getText().toString());
-                                    user.setPass(passwdText.getText().toString());
-                                    db.addUser(user);
-                                    Toast.makeText(AuthActivity.this, "User was registered.", Toast.LENGTH_LONG).show();
-                                } else
-                                    Toast.makeText(AuthActivity.this, "User with this login already exists.", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-                }).start();
+            public void run() {
+                try {
+                    cameraProvider = cameraProviderListenableFuture.get();
+                    startCamera(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        });
+        }, ContextCompat.getMainExecutor(this));
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        db.close();
+    private void capturePhoto() {
+        if (imageCapture == null) return;
+
+        String name = System.currentTimeMillis() + "";
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraXStable");
+        }
+
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues).build();
+        imageCapture.takePicture(outputFileOptions,
+                ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(AuthActivity.this,
+                                "Сохранено в " + outputFileResults.getSavedUri(),
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(AuthActivity.this,
+                                "Что то пошло не так",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void startCamera(ProcessCameraProvider cameraProvider) {
+        CameraSelector defaultFrontCamera = CameraSelector.DEFAULT_FRONT_CAMERA;
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(activityBinding.viewFinder.getSurfaceProvider());
+        imageCapture = new ImageCapture.Builder().build();
+        try {
+            cameraProvider.unbindAll();
+            cameraProvider.bindToLifecycle(this, defaultFrontCamera, preview, imageCapture);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
